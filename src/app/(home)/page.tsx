6da6af098e185c36933e4e9742afc78f358d6930
MAC <ronyortiz@MACs-iMac.local>
2025-02-17 18:32:02 -0400

    webhook sync complete

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,6 @@
 const HomePage = () => {
-  return <div>I will load videos in the future</div>;
+  return (
+    <div>I will load videos in the future, I have more money than I need. </div>
+  );
 };
 export default HomePage;
